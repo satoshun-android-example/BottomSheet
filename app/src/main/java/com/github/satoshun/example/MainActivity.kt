99a73304603802713edc498682812e6d6fe6229c
SatoShun <shun.sato1@gmail.com>
2019-07-25 10:19:04 +0000

    transition

--- app/src/main/java/com/github/satoshun/example/MainActivity.kt
+++ app/src/main/java/com/github/satoshun/example/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.github.satoshun.example
 
+import android.animation.LayoutTransition
+import android.content.Context
 import android.os.Bundle
 import android.view.View
+import androidx.annotation.Px
 import androidx.appcompat.app.AppCompatActivity
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import com.github.satoshun.example.databinding.MainActBinding
 import com.google.android.material.bottomsheet.BottomSheetBehavior
@@ -36,5 +40,24 @@ class MainActivity : AppCompatActivity() {
         behavior.state = BottomSheetBehavior.STATE_EXPANDED
       }
     }
+
+    binding.title2.setOnClickListener {
+      binding.goneable.isVisible = !binding.goneable.isVisible
+      if (binding.goneable.isVisible) {
+        behavior.peekHeight = dpToPx(120 + 100)
+      } else {
+        behavior.peekHeight = dpToPx(120)
+      }
+    }
+
+    val transition = LayoutTransition()
+    transition.setAnimateParentHierarchy(false)
+    binding.bottom.layoutTransition = transition
   }
 }
+
+
+@Px
+private fun Context.dpToPx(
+  value: Int
+): Int = (value * this.resources.displayMetrics.density).toInt()
