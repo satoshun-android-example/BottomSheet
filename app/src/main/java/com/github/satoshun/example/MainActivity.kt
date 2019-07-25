8cf9e7ef34566fb161e045fdba37a395c7b48b0d
SatoShun <shun.sato1@gmail.com>
2019-07-25 01:51:56 +0000

    handle bottom sheet

--- app/src/main/java/com/github/satoshun/example/MainActivity.kt
+++ app/src/main/java/com/github/satoshun/example/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.github.satoshun.example
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.databinding.DataBindingUtil
 import com.github.satoshun.example.databinding.MainActBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class MainActivity : AppCompatActivity() {
   private lateinit var binding: MainActBinding
@@ -11,5 +14,17 @@ class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     binding = DataBindingUtil.setContentView(this, R.layout.main_act)
+
+    val params = binding.bottom.layoutParams as CoordinatorLayout.LayoutParams
+    val behavior = params.behavior as BottomSheetBehavior<View>
+    behavior.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+      override fun onSlide(bottomSheet: View, slideOffset: Float) {
+        println("onSlide $slideOffset")
+      }
+
+      override fun onStateChanged(bottomSheet: View, newState: Int) {
+        println("onStateChanged $newState")
+      }
+    })
   }
 }
