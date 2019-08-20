08ca250b3e47c95e799faa86959832240f961172
SatoShun <shun.sato1@gmail.com>
2019-08-20 07:46:24 +0000

    use fitssystem

--- app/src/main/java/com/github/satoshun/example/MainActivity.kt
+++ app/src/main/java/com/github/satoshun/example/MainActivity.kt
@@ -18,6 +18,10 @@ class MainActivity : AppCompatActivity() {
   private lateinit var binding: MainActBinding
 
   override fun onCreate(savedInstanceState: Bundle?) {
+    window.decorView.systemUiVisibility = window.decorView.systemUiVisibility or
+      View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+      View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+
     super.onCreate(savedInstanceState)
     binding = DataBindingUtil.setContentView(this, R.layout.main_act)
 
